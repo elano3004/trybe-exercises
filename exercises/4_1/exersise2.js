3e7e2ede7deb5f0e48f3a77616dca51c2ae4f9c7
Elano Evaristo <elano3004@gmail.com>
2020-08-27 20:02:56 -0300

    exercicio 02 - programa de comparacao de 2 numeros

---
+++ exercises/4_1/exersise2.js
@@ -0,0 +1,14 @@
+//Programa que compara dois valores
+
+var x = 5;
+var y = 25;
+
+//Comparando
+
+console.log("Vamos ver qual é maior?");
+console.log("A=" +x+ " e B=" +y);
+if( x > y ){
+  console.log('A é maior'); 
+} else {
+  console.log("B é maior");
+}

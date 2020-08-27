ab343f8d894dfbf11a01ef58afbe5253fc154bb2
Elano Evaristo <elano3004@gmail.com>
2020-08-27 20:12:21 -0300

    exercicio 03 - programa de comparacao de 3 numeros

---
+++ exercises/4_1/exersise3.js
@@ -0,0 +1,16 @@
+//Programa que compara tres valores
+
+var x = 45;
+var y = 25;
+var z = 35
+
+//Comparando
+
+console.log("Vamos ver qual é maior?");
+console.log("A=" +x+ " B=" +y+ " C=" +z);
+if( x > y && x > z)
+  console.log("A é maior"); 
+else if (y > z) 
+  console.log("B é maior");
+else
+  console.log("C é maior");

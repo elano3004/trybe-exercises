ea1e7a1dadb305e3fd692fcad2dfff5c67dbca69
Elano Evaristo <elano3004@gmail.com>
2020-08-28 13:21:38 -0300

    exercicio 10 - programa verifica calcula lucro

---
+++ exercises/4_1/exersise10.js
@@ -0,0 +1,20 @@
+//Programa que calcula lucro
+
+let valorCusto = 10;
+let valorVenda = 50;
+let imposto = (valorCusto * 0.2);
+let valorTotal = valorCusto + imposto;
+let lucro = valorVenda - valorTotal;
+let lucroMil = (lucro * 1000);
+
+// //verificando se tem um numero impar ou não
+
+if (valorCusto < 0 || valorVenda < 0) {
+  console.log("false");
+} else {
+  console.log("> Valor de compra do produto: " +valorCusto);
+  console.log("> Valor do imposto do produto: " +imposto);
+  console.log("> Valor de Total do produto + imposto: " +valorTotal);
+  console.log("> Valor do lucro em um produto: " +lucro);
+  console.log("> Valor do lucro em uma venda de mil produtos: " +lucroMil);
+}

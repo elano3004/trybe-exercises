9a8160e5985d8d7b04581daa14cc35c6017f7148
Elano Evaristo <elano3004@gmail.com>
2020-08-28 18:06:14 -0300

    Resolvendo exersise 1

---
+++ exercises/4_2/exersise1.js
@@ -0,0 +1,8 @@
+// Programa que exibe um array
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+console.log(numbers);
+
+// for(let indice = 0; indice < numbers.length; indice += 1) {
+//   console.log(`Números do array são: ${numbers[indice]}`);
+// }

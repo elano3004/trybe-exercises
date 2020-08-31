a89fe9a0d3776be677dbf510cac27ed66536546a
Elano Evaristo <elano3004@gmail.com>
2020-08-31 18:20:33 -0300

    subindo restante dos exersises

---
+++ exercises/4_2/exersise5.js
@@ -0,0 +1,11 @@
+// Programa que descobre qual he o maior
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let higherNumber = 0;
+
+for (let i = 0; i < numbers.length; i++) {
+  if (numbers[i] > higherNumber) {
+    higherNumber = numbers[i];
+  }
+}
+
+console.log(higherNumber);
---
+++ exercises/4_2/exersise9.js
@@ -0,0 +1,3 @@
+for (let i = 0; i < numbers.length; i += 1) {
+  console.log(numbers[i] / 2);
+};

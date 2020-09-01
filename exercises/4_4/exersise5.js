7db83159a26ce0a7bcce93d83ab07ba2167384ef
Elano Evaristo <elano3004@gmail.com>
2020-09-01 19:13:45 -0300

    exercise5 - for/in

---
+++ exercises/4_4/exersise5.js
@@ -0,0 +1,29 @@
+// Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do 
+// primeiro e os seguintes valores: "Tio Patinhas", "Christmas on Bear Mountain, 
+// Dell's Four Color Comics #178", "O último MacPatinhas", "Sim".
+
+let info = {
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+  recorrente: "Sim"
+};
+
+let info2 = {
+  personagem: 'Tio Patinhas',
+  origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+  nota: 'O último MacPatinhas',
+  recorrente: "Sim"
+};
+
+for(let propriedades in info){
+  if(
+    info[propriedades] === info.recorrente &&
+    info[propriedades] === "Sim" &&
+    info2[propriedades] === "Sim"
+  ) {
+    console.log("Ambos são recorrentes");  
+  } else {
+    console.log(info[propriedades] + " e " + info2[propriedades]);
+  }
+}

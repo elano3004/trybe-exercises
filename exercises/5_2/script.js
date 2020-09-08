c53aae445827ae6bb9a6460c93d479c7e26d4195
Elano Evaristo <elano3004@gmail.com>
2020-09-08 18:50:43 -0300

    Questão N#1

---
+++ exercises/5_2/script.js
@@ -0,0 +1,6 @@
+let body = document.querySelector('body')
+let title = document.createElement('h1');
+title.innerText = "Exercício 5.2 - JavaScript DOM";
+console.log(title);
+
+body.appendChild(title);

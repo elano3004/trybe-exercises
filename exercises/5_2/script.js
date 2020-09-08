70c467141e0792d19f9507ef82ba5dbf156c2394
Elano Evaristo <elano3004@gmail.com>
2020-09-08 19:22:32 -0300

    Questão N#2

--- exercises/5_2/script.js
+++ exercises/5_2/script.js
@@ -1,6 +1,14 @@
-let body = document.querySelector('body')
+//Questão 01
+//Segmentação a tag h1com o texto Exercício 5.2 - JavaScript DOMcomo filho da tag body;
+let body = document.body;
 let title = document.createElement('h1');
+title.classList.add('title');
 title.innerText = "Exercício 5.2 - JavaScript DOM";
 console.log(title);
-
 body.appendChild(title);
+
+//Questão 02
+//Pesquisar a tag divcom a classe main-contentcomo filho da tag body;
+let div = document.createElement('div');
+div.classList.add('main-content'); 
+body.appendChild(div);
